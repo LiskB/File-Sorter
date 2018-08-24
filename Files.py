0b2d5e1ee146d05c613404df5f62c76510e7c030
LiskB <brandenlisk@gmail.com>
2018-08-24 16:03:21 -0400

    Added functionality to sort by file type

--- Files.py
+++ Files.py
@@ -6,15 +6,19 @@ import shutil
 
 
 ## DEFINE FUNCTIONS AND VARIABLES
-dirPath=""
+dirPath="" # Stores the path the user chooses
 
+## This method opens the prompt dialog box for the user to choose a directory
 def openDirectory():
   global dirPath
   dirPath = askdirectory(parent=root, initialdir="/", mustexist=True, title = "Choose a directory to organize")
   pathLabel.config(text=dirPath)
   
+## This method brings all files to the parent directory, deletes the empty folders
+## Also has the option to group files into new folders, based on user selection
 def organizeFiles():
   global dirPath
+  
   if os.path.exists(dirPath):
     fileList = []
     for root, _, filenames in os.walk(dirPath):
@@ -25,17 +29,30 @@ def organizeFiles():
     # Add all files to parent directory
     for filepath in fileList:
       shutil.move(filepath, os.path.join(dirPath, os.path.basename(filepath)))
-    print("move complete")
     # Delete the empty folders
     childrenPaths = os.listdir(dirPath)
     for childPath in childrenPaths:
-      print ("Check path: " + childPath)
       if os.path.isdir(os.path.join(dirPath, childPath)):
-        print ("is path")
         shutil.rmtree(os.path.join(dirPath, childPath))
+    
+    # Check for organization method
+    global typeVar
+    if typeVar.get() == 1:
+      fileList = os.listdir(dirPath)
+      for item in fileList:
+        name, extension = os.path.splitext(os.path.join(dirPath, item))
+        extensionName = extension[1:].upper() # remove period from beginning of name, put in uppercase
+        newPath = os.path.join(dirPath, extensionName) # define new path for file
+        if not os.path.isdir(newPath):
+          # if directory doesn't exist, create it
+          os.makedirs(newPath)
+        # add file to folder
+        shutil.move(os.path.join(dirPath, item), os.path.join(newPath, item))
+  
   else:
-    print ("path does not exist")
+    pathLabel.config(text="Error: Path not found")
 
+## This method exits the program
 def client_exit():
   exit()
 
@@ -66,6 +83,13 @@ browseButton.pack()
 
 pathLabel = Label(root)
 pathLabel.pack()
+
+organizeLabel = Label(root, text="Organization method: ")
+organizeLabel.pack()
+
+typeVar = IntVar()
+typeCheck = Checkbutton(root, text="Sort by file type", variable=typeVar)
+typeCheck.pack()
 
 organizeButton = Button(root, text="Organize", command= organizeFiles)
 organizeButton.pack()
